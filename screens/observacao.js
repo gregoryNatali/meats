658ed43eabf8247495d0e6566db8497c56d0b02b
jhbertelli <joao_h_bertelli@estudante.sc.senai.br>
2022-06-20 09:11:07 -0300

    adicionando js para as telas de observacao e avaliar

---
+++ screens/observacao.js
@@ -0,0 +1,8 @@
+let inputObs = document.querySelector('#input_observacao')
+
+inputObs.addEventListener('keyup', () => {
+    const counter = document.querySelector('#input-counter')
+    let observacao = document.querySelector('#input_observacao').value
+    
+    counter.textContent = 100 - observacao.length
+})
---
+++ screens/stars.js
@@ -0,0 +1,15 @@
+const starsDiv = document.querySelector('#estrelas')
+
+for (let star = 0; star < starsDiv.childElementCount; star++) {
+    starsDiv.children[star].style.fill = "#F9DF9C"
+    starsDiv.children[star].addEventListener('click', () => {
+        let nota = star + 1
+        console.log(nota)
+        for (let reset = 0; reset < 5; reset++) {
+            starsDiv.children[reset].style.fill = "#F9DF9C"
+        }
+        for (let index = 0; index < nota; index++) {
+            starsDiv.children[index].style.fill = "#D79B00"
+        }
+    })
+}
